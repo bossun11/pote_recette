11dda746645cc43cde562d50ac0f19ab7d2afdda
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-22 17:56:40 +0900

    Add: ESLintとPrettierのインストールと初期設定を実施

---
+++ front/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "no-unused-vars": "warn", //　未使用の変数があったら警告
+    eqeqeq: "error", // '==='と'!=='を強制
+    curly: "error", // 制御文(if, while etc)のブロックを強制
+    semi: ["error", "always"], // セミコロンを強制
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
