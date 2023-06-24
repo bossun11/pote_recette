fb91a98acefe6a738f8f19732dd4e9cdc8c272d1
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-24 13:11:52 +0900

    Add: daisy UIをインストール

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
