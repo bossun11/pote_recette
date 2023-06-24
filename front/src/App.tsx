33b5b65e817f165b814018d3f0c74868ba5a2e4e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-24 12:58:40 +0900

    Add: Tailwind CSSをインストール

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">React on Rails</div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
