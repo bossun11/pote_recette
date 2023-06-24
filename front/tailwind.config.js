87f27e86cfa9bf9801f3c5356d8d948b96952be4
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-24 16:28:19 +0900

    Add: 使用するcolorsプロパティを設定

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D08C8A",
+        secondary: "#F6F1E6",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
