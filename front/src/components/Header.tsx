71849596fef6b7dd9b6c9da9bec9c83854497fdb
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-26 16:47:48 +0900

    Fix: メニューアイコンを修正

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -2,41 +2,28 @@ import React, { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className="bg-primary py-2 px-4">
+    <nav className="bg-primary py-2 px-6">
       <div className="flex items-center justify-between">
-        <div className="text-white text-lg">Your Logo</div>
-        <div className="swap" onClick={() => setIsOpen(!isOpen)}>
-          <label className="btn btn-circle swap swap-rotate">
-            {/* this hidden checkbox controls the state */}
-            <input type="checkbox" />
-
-            {/* hamburger icon */}
-            <svg
-              className="swap-off fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 512 512"
-            >
-              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-            </svg>
-
-            {/* close icon */}
-            <svg
-              className="swap-on fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 512 512"
-            >
-              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-            </svg>
-          </label>
-        </div>
+        <div className="text-white text-2xl">Logo</div>
+        <button type="button" className="text-white" onClick={() => setIsOpen(!isOpen)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
