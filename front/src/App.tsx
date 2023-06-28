af5245137b5498f7e0a049f088a86adf1c82c4bf
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-28 14:36:34 +0900

    Add: ログインページへのパスを設定

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Login />
-    </>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
