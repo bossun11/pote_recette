e3ed6971086fa792c715fe85b9f72938694ccf3f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-29 16:24:53 +0900

    Add: react-hook-formを使ったログインフォームの入力欄の状態を管理するコードを追加

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onChange" });
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-96 bg-white rounded p-6 shadow-xl">
         <h2 className="text-2xl text-center font-bold mb-5 text-gray-800">ログイン画面</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label className="mb-2 text-sm" htmlFor="email">
               メールアドレス
@@ -15,7 +31,12 @@ const Login = () => {
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="email"
               type="email"
+              {...register("email", {
+                required: "Emailが入力されていません。",
+                minLength: { value: 4, message: "4文字以上で入力してください。" },
+              })}
             />
+            <p className="text-error">{errors.email?.message as React.ReactNode}</p>
           </div>
           <div className="mb-5">
             <label className="mb-2 text-sm" htmlFor="password">
@@ -25,7 +46,12 @@ const Login = () => {
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="password"
               type="password"
+              {...register("password", {
+                required: "パスワードが入力されていません。",
+                minLength: { value: 8, message: "8文字以上で入力してください。" },
+              })}
             />
+            <p className="text-error">{errors.password?.message as React.ReactNode}</p>
           </div>
           <div className="flex justify-center">
             <button className="btn btn-active btn-primary">ログイン</button>
