18f3f9d7d7a8b026f8a182b110da24455198e623
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-30 10:45:51 +0900

    Add: ユーザー登録ページにフォームの状態管理とバリデーションを追加

--- front/src/components/Register/Register.tsx
+++ front/src/components/Register/Register.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerValidationSchema } from "../../utils/validationSchema";
+
+type RegisterForm = z.infer<typeof registerValidationSchema>;
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterForm>({ mode: "onChange", resolver: zodResolver(registerValidationSchema) });
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data: RegisterForm) => {
+    console.log(data);
+    navigate("/login");
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-96 bg-white rounded p-6 shadow-xl">
         <h2 className="text-2xl text-center font-bold mb-5 text-gray-800">ユーザー登録画面</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label className="mb-2 text-sm" htmlFor="name">
               ユーザー名
@@ -14,7 +34,9 @@ const Register = () => {
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="name"
               type="text"
+              {...register("name")}
             />
+            {errors.name && <span className="text-error">{errors.name.message}</span>}
           </div>
           <div className="mb-2">
             <label className="mb-2 text-sm" htmlFor="email">
@@ -24,7 +46,9 @@ const Register = () => {
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="email"
               type="email"
+              {...register("email")}
             />
+            {errors.email && <span className="text-error">{errors.email.message}</span>}
           </div>
           <div className="mb-2">
             <label className="mb-2 text-sm" htmlFor="password">
@@ -34,7 +58,9 @@ const Register = () => {
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="password"
               type="password"
+              {...register("password")}
             />
+            {errors.password && <span className="text-error">{errors.password.message}</span>}
           </div>
           <div className="mb-5">
             <label className="mb-2 text-sm" htmlFor="passwordConfirmation">
@@ -43,8 +69,12 @@ const Register = () => {
             <input
               className="w-full px-3 py-2 border rounded-md outline-none"
               id="passwordConfirmation"
-              type="passwordConfirmation"
+              type="password"
+              {...register("passwordConfirmation")}
             />
+            {errors.passwordConfirmation && (
+              <span className="text-error">{errors.passwordConfirmation.message}</span>
+            )}
           </div>
           <div className="flex justify-center">
             <button className="btn btn-active btn-primary">登録する</button>
--- front/src/utils/validationSchema.ts
+++ front/src/utils/validationSchema.ts
@@ -11,3 +11,26 @@ export const loginValidationSchema = z.object({
     .min(8, "8文字以上で入力してください")
     .max(50, "50文字以下で入力してください"),
 });
+
+export const registerValidationSchema = z
+  .object({
+    name: z.string().nonempty("名前は必須です").max(50, "50文字以下で入力してください"),
+    email: z
+      .string()
+      .nonempty("メールアドレスは必須です")
+      .email("正しいメールアドレスを入力してください"),
+    password: z
+      .string()
+      .nonempty("パスワードは必須です")
+      .min(8, "8文字以上で入力してください")
+      .max(50, "50文字以下で入力してください"),
+    passwordConfirmation: z
+      .string()
+      .nonempty("パスワード(確認)は必須です")
+      .min(8, "8文字以上で入力してください")
+      .max(50, "50文字以下で入力してください"),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "パスワードと確認用パスワードが一致していません",
+    path: ["passwordConfirmation"],
+  });
