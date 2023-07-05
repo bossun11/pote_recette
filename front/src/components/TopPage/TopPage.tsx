3d942bce2f1cdf2f2f0b1b353e9e44d9ec1d66df
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-05 13:13:34 +0900

    Add: TOPページを作成

---
+++ front/src/components/TopPage/TopPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NeutralButton from "../Buttons/NeutralButton";
+import { Link } from "react-router-dom";
+
+const TopPage = () => {
+  const BTNTEXT = "ログインして始める";
+
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 bg-neutral-content opacity-20"></div>
+      <img className="w-full h-screen object-cover" src="/images/top_image.jpg" alt="" />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <img src="/images/logo.png" alt="" />
+        <p className="text-white text-2xl pb-5">お芋専門店を気軽に探せるアプリです</p>
+        <Link to="/login">
+          <NeutralButton BTNTEXT={BTNTEXT} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TopPage;
