ba5ab351978737294cf7edaf4980b98e6fb347b1
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-06 15:14:03 +0900

    Add: deviseを使ったユーザー登録・ログイン機能を作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations',
+      }
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
+  end
 end
