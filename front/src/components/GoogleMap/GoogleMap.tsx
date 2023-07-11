05cc59beae503d3b6891fc8f45ec62d167bd4c00
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-11 23:45:39 +0900

    Fix: LoadScriptからuseJsApiLoaderを使った記述に変更
    LoadScriptのコードだと一度レンダリングした後、別コンポーネントに移動し再度GoogleMapコンポーネントを呼ぶとコンソールでエラーが発生しLoading画面のままになってしまうためuseJsApiLoaderの記述に変更した

--- front/src/components/GoogleMap/GoogleMap.tsx
+++ front/src/components/GoogleMap/GoogleMap.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { GoogleMap as GoogleMapComponent, LoadScript, MarkerF } from "@react-google-maps/api";
+import { GoogleMap as GoogleMapComponent, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 type CenterType = {
   lat: number;
@@ -11,24 +11,29 @@ type ContentStyleType = {
   height: string;
 };
 
+const containerStyle: ContentStyleType = {
+  width: "100%",
+  height: "100%",
+};
+
+// とりあえず名古屋城を中心に表示
+const center: CenterType = {
+  lat: 35.182253007459444,
+  lng: 136.90534328438358,
+};
+
 const GoogleMap: FC = () => {
-  const containerStyle: ContentStyleType = {
-    width: "100%",
-    height: "100%",
-  };
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || "",
+  });
 
-  // とりあえず名古屋城を中心に表示
-  const center: CenterType = {
-    lat: 35.182253007459444,
-    lng: 136.90534328438358,
-  };
-
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || ""}>
-      <GoogleMapComponent mapContainerStyle={containerStyle} center={center} zoom={15}>
-        <MarkerF position={center} />
-      </GoogleMapComponent>
-    </LoadScript>
+  return isLoaded ? (
+    <GoogleMapComponent mapContainerStyle={containerStyle} center={center} zoom={15}>
+      <Marker position={center} />
+    </GoogleMapComponent>
+  ) : (
+    <></>
   );
 };
 
