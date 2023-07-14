2de76361c8b224915a64a102efb22bda66a1034a
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-14 13:16:01 +0900

    Add: カード内にショップの画像を追加

--- front/src/components/ShopSearch/ShopSearch.tsx
+++ front/src/components/ShopSearch/ShopSearch.tsx
@@ -14,6 +14,9 @@ type ShopType = {
   place_id: string;
   name: string;
   formatted_address: string;
+  photos: {
+    photo_reference: string;
+  }[];
 };
 
 const ShopSearch: FC = () => {
@@ -44,11 +47,19 @@ const ShopSearch: FC = () => {
   // ショップの情報をカードにして表示
   const renderShopCard = (shop: ShopType) => {
     const address = shop.formatted_address.replace(/日本、〒\d{3}-\d{4} /, "");
+    const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=${shop.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`;
     return (
-      <div key={shop.place_id} className="card w-88 bg-base-100">
-        <div className="card-body ">
-          <h2 className="card-title">{shop.name}</h2>
-          <p>{address}</p>
+      <div key={shop.place_id} className="card w-88 bg-base-100 cursor-pointer">
+        <div className="card-body">
+          <div className="flex justify-between items-center">
+            <div className="flex-col ite">
+              <h2 className="card-title">{shop.name}</h2>
+              <p>{address}</p>
+            </div>
+            <div className="items-center">
+              <img src={photoUrl} alt="" className="w-20 h-20" />
+            </div>
+          </div>
         </div>
       </div>
     );
