bfb96672ce09c0d2435c235a6e88c5ba16e7532c
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-18 20:37:49 +0900

    Add: user, bookmark, shopモデルに対応するリレーションを定義

--- api/app/models/shop.rb
+++ api/app/models/shop.rb
@@ -1,4 +1,7 @@
 class Shop < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarked_by_users, through: :bookmarks, source: :user
+
   validates :name, :formatted_address, :place_id, presence: true
   validates :place_id, uniqueness: true
 end
