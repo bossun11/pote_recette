64a53c028fbb479604c97a97e9bc4d021f558b35
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-19 17:21:42 +0900

    MiniMagickのインストールと画像の自動リサイズを設定

--- api/app/uploaders/photo_uploader.rb
+++ api/app/uploaders/photo_uploader.rb
@@ -1,7 +1,9 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
