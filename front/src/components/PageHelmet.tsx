649237895410d0e5318e7490fb1a30b39e4b12ce
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-07-24 10:15:16 +0900

    Add: OGP設定を行った

--- front/src/components/PageHelmet.tsx
+++ front/src/components/PageHelmet.tsx
@@ -3,12 +3,22 @@ import { Helmet } from "react-helmet-async";
 
 type PageHelmetProps = {
   title?: string;
+  description?: string;
 };
 
-const PageHelmet: FC<PageHelmetProps> = ({ title = "" }) => {
+const appName = "PotaRecette";
+
+const PageHelmet: FC<PageHelmetProps> = ({ title = "", description = "" }) => {
   return (
-    <Helmet title={title ? `${title} | PotaRecette` : undefined} defaultTitle="PotaRecette">
+    <Helmet title={title ? `${title} | ${appName}` : undefined} defaultTitle={appName}>
       <link rel="icon" href="/images/favicon.ico" />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={appName} />
+      <meta name="twitter:title" content="summary_large_image" />
+      {/* 下記はデプロイしていないので仮のURLを設定 */}
+      <meta property="og:url" content="http://localhost:3000/" />
+      <meta property="og:image" content="http://localhost:3000/images/ogp.png" />
     </Helmet>
   );
 };
