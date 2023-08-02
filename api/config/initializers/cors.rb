df457afa7ebaa17d2e735395631bf6e62aba2d0e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-02 17:57:36 +0900

    Fix: Herokuアプリを作り直したのでURLを修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "https://potarecette-front-ff9b017b0030.herokuapp.com"
+    origins "localhost:3000", "potarecette-front-a29fcf2f5b8d.herokuapp.com"
 
     resource "*",
       headers: :any,
