abdb29a33a6b2d862fe5a9b1f026a386f5c8090a
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-02 17:12:48 +0900

    Fix: 画像をAWS S3へアップロードするように修正

--- api/app/uploaders/photo_uploader.rb
+++ api/app/uploaders/photo_uploader.rb
@@ -6,8 +6,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
