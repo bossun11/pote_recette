f962af1e52b5b4373aadd574f4a4de8a8aa30aea
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-02 11:45:06 +0900

    Fix: corsに設定したURLが間違っていた為修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "http://potarecette-api-d39f55a08d27.herokuapp.com"
+    origins "localhost:3000", "https://potarecette-front-ff9b017b0030.herokuapp.com"
 
     resource "*",
       headers: :any,
