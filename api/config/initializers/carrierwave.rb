5b27eb3b0297bd5fb27be966b3d0ff4e763bf7d9
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-03 23:29:58 +0900

    Fix: AWS S3へアップロードした画像が正しく表示されないバグを修正

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'potarecette-shop-images'
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      region: 'us-east-1',
       path_style: true
     }
 end
