33a8492993994a2a770fc36a40b0ed2a481f412f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-04 22:56:37 +0900

    Fix: titleの修正箇所をミス

--- front/src/components/PageHelmet.tsx
+++ front/src/components/PageHelmet.tsx
@@ -8,12 +8,12 @@ type PageHelmetProps = {
 
 const appName = "PotaRecette";
 
-const PageHelmet: FC<PageHelmetProps> = ({ title = appName, description = "" }) => {
+const PageHelmet: FC<PageHelmetProps> = ({ title = "", description = "" }) => {
   return (
     <Helmet title={title ? `${title} | ${appName}` : undefined} defaultTitle={appName}>
       <link rel="icon" href="/images/favicon.ico" />
       <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={appName} />
       <meta property="og:site_name" content={appName} />
       <meta name="twitter:title" content="summary_large_image" />
       <meta property="og:url" content={process.env.REACT_APP_HEROKU_FRONTEND_APP_URL} />
