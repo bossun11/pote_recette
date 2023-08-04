4ff6f5b8226a37f0071ed6ac974292e68c7548ad
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-05 00:40:39 +0900

    Fix: HelmetProviderの呼び出し位置をindex.tsxに変更

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import { HelmetProvider } from "react-helmet-async";
 
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,19 +9,17 @@ import AppRoutes from "./AppRoutes";
 
 const App = () => {
   return (
-    <HelmetProvider>
-      <AuthProvider>
-        <ShopProvider>
-          <ToastContainer
-            position="bottom-center"
-            draggable={false}
-            pauseOnHover={false}
-            theme="colored"
-          />
-          <AppRoutes />
-        </ShopProvider>
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <ShopProvider>
+        <ToastContainer
+          position="bottom-center"
+          draggable={false}
+          pauseOnHover={false}
+          theme="colored"
+        />
+        <AppRoutes />
+      </ShopProvider>
+    </AuthProvider>
   );
 };
 
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
