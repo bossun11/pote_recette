f85e936ae878fe52199232da3029c8b45599f6a2
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-05 14:18:46 +0900

    Change: HelmetProviderのimport箇所をApp.tsxに戻した

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/AuthContext";
 import { ShopProvider } from "./context/ShopContext";
+import { HelmetProvider } from "react-helmet-async";
 import AppRoutes from "./AppRoutes";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <ShopProvider>
-        <ToastContainer
-          position="bottom-center"
-          draggable={false}
-          pauseOnHover={false}
-          theme="colored"
-        />
-        <AppRoutes />
-      </ShopProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <ShopProvider>
+          <ToastContainer
+            position="bottom-center"
+            draggable={false}
+            pauseOnHover={false}
+            theme="colored"
+          />
+          <AppRoutes />
+        </ShopProvider>
+      </AuthProvider>
+    </HelmetProvider>
   );
 };
 
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 
 import "./index.css";
 import App from "./App";
@@ -8,8 +7,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
 );
