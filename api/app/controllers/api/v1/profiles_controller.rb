02a729ed2de1d803ab6b379100caa3e322c748cb
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-09 22:08:13 +0900

    Add: before_action・エラーメッセージ・ストロングパラメータを追加

--- api/app/controllers/api/v1/profiles_controller.rb
+++ api/app/controllers/api/v1/profiles_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::ProfilesController < ApplicationController
+  before_action :authenticate_api_v1_user!
 
   def update
     user = current_api_v1_user
     if user.update(user_params)
       render json: { status: 200, data: user }
     else
-      render json: { status: 500 }
+      render json: { status: 500, errors: user.errors.full_messages }
     end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email, :image)
+    params.require(:user).permit(:name, :email, :image)
   end
 end
