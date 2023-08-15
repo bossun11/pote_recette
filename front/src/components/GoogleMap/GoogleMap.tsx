c46679500d15b858c64ac1876047b53eab12b0dc
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-15 15:09:58 +0900

    Fix: マーカーをpropsとして渡すように修正
    この修正によりショップ詳細ページでリロードしてもマーカーが消滅しないように修正した

--- front/src/components/GoogleMap/GoogleMap.tsx
+++ front/src/components/GoogleMap/GoogleMap.tsx
@@ -2,11 +2,12 @@ import React, { FC } from "react";
 import { GoogleMap as GoogleMapComponent, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 import { GoogleMapCenterType } from "../../types";
-import { useShopContext } from "../../context/ShopContext";
+import { ShopType } from "../../types";
 
 type GoogleMapProps = {
   center: GoogleMapCenterType;
   zoom?: number;
+  markers: ShopType[] | ShopType;
 };
 
 type ContentStyleType = {
@@ -19,19 +20,19 @@ const containerStyle: ContentStyleType = {
   height: "100%",
 };
 
-const GoogleMap: FC<GoogleMapProps> = ({ center, zoom }) => {
+const GoogleMap: FC<GoogleMapProps> = ({ center, zoom, markers }) => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || "",
   });
 
-  const { shops } = useShopContext();
-
   return isLoaded ? (
     <GoogleMapComponent mapContainerStyle={containerStyle} center={center} zoom={zoom}>
-      {shops.map((shop) => (
-        <Marker key={shop.place_id} position={shop.geometry.location} />
-      ))}
+      {Array.isArray(markers) ? (
+        markers.map((shop) => <Marker key={shop.place_id} position={shop.geometry.location} />)
+      ) : (
+        <Marker key={markers.place_id} position={markers.geometry.location} />
+      )}
     </GoogleMapComponent>
   ) : (
     <></>
