0ae4ae8e5a376ce545670ded201bf4420e5feeca
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-30 13:43:26 +0900

    Delete: 今回実装したオートコンプリート機能に関するコードを削除

--- front/src/components/ShopSearch/SearchForm.tsx
+++ front/src/components/ShopSearch/SearchForm.tsx
@@ -4,19 +4,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FiX } from "react-icons/fi";
 import { inputSearchValidationSchema } from "../../utils/validationSchema";
 import { InputSearchParams } from "../../types";
-import Autocomplete from "react-google-autocomplete";
 
 type SearchFormProps = {
   onSubmit: (_data: InputSearchParams) => void; // eslint-disable-line no-unused-vars
 };
 
 const SearchForm: FC<SearchFormProps> = ({ onSubmit }) => {
-  const { register, handleSubmit, formState, reset, setValue } = useForm<InputSearchParams>({
+  const { register, handleSubmit, formState, reset } = useForm<InputSearchParams>({
     mode: "onChange",
     resolver: zodResolver(inputSearchValidationSchema),
   });
-
-  const { ref, ...inputProps } = register("search");
 
   const handleReset = () => {
     reset();
@@ -26,20 +23,11 @@ const SearchForm: FC<SearchFormProps> = ({ onSubmit }) => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex justify-center items-center">
         <div className="relative">
-          <Autocomplete
-            apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
-            onPlaceSelected={(place) => {
-              setValue("search", place.name || "");
-            }}
-            options={{
-              types: ["establishment"],
-              fields: ["name"],
-              componentRestrictions: { country: "jp" },
-            }}
-            ref={ref}
+          <input
+            type="text"
             placeholder="検索"
             className="input input-bordered w-full max-w-xs"
-            {...inputProps}
+            {...register("search")}
           />
           {formState.isValid && (
             <FiX
