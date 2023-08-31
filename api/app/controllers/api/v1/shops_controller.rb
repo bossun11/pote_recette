40b00d4189c0e5b525b31b617a0961a3d3189968
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-08-31 14:42:30 +0900

    Fix: 現在地付近のショップのみを表示するように修正

--- api/app/controllers/api/v1/shops_controller.rb
+++ api/app/controllers/api/v1/shops_controller.rb
@@ -4,8 +4,16 @@ class Api::V1::ShopsController < ApplicationController
   require "cgi"
 
   def search
-    query = CGI.escape("さつまいも菓子専門店+in+#{params[:location]}")
-    uri = URI.parse("#{ENV['GOOGLE_MAP_PLACE_URL']}/textsearch/json?query=#{query}&key=#{ENV['GOOGLE_MAP_API_KEY']}&language=ja")
+    if params[:location] =~ /^\d+\.\d+,\d+\.\d+$/ # 緯度経度の形式かどうかチェック
+      # 緯度経度が与えられた場合
+      query = CGI.escape("さつまいも菓子専門店")
+    else
+      # 地名や店名が与えられた場合
+      query = CGI.escape("さつまいも菓子専門店+in+#{params[:location]}")
+    end
+
+    radius = params[:radius] || 10000
+    uri = URI.parse("#{ENV['GOOGLE_MAP_PLACE_URL']}/textsearch/json?query=#{query}&key=#{ENV['GOOGLE_MAP_API_KEY']}&language=ja&radius=#{radius}")
     res = Net::HTTP.get_response(uri)
     render json: res.body
   end
