8557a84cbdaa05a8fdc8b9e5c0197cc478c5fcfe
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-09-09 15:52:13 +0900

    Add: パスワードリセット用のルートを追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
+        passwords: 'api/v1/auth/passwords',
       }
       namespace :auth do
         resources :sessions, only: %i[index]
