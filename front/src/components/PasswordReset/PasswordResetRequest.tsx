52a7c88c3efe1e204f80dfa9233e5bdd3af6f82d
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-09-12 14:58:25 +0900

    Add: パスワードリセットメール送信ページのデザインを追加

--- front/src/components/PasswordReset/PasswordResetRequest.tsx
+++ front/src/components/PasswordReset/PasswordResetRequest.tsx
@@ -1,7 +1,37 @@
 import React, { FC } from "react";
+import PageHelmet from "../PageHelmet";
+import NeutralButton from "../Buttons/NeutralButton";
 
 const PasswordResetRequest: FC = () => {
-  return <div>PasswordResetRequest</div>;
+  return (
+    <>
+      <PageHelmet title="パスワードリセット" />
+      <div className="flex items-start justify-center mt-24">
+        <div className="card w-96 bg-white">
+          <div className="card-body">
+            <h2 className="card-title text-2xl mb-5 flex items-center justify-center">
+              パスワードリセット
+            </h2>
+            <form>
+              <div className="mb-3">
+                <label className="mb-2 text-sm" htmlFor="email">
+                  メールアドレス
+                </label>
+                <input
+                  className="w-full px-3 py-2 border rounded-md outline-none"
+                  id="email"
+                  type="email"
+                />
+              </div>
+              <div className="card-actions justify-center">
+                <NeutralButton buttonText="メールを送信する" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PasswordResetRequest;
