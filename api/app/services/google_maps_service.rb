0caf645db3df616357d46b0e4001c7061d24cd9b
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-09-22 14:14:38 +0900

    Refactor: shops_controllerのAPI処理をServiceクラスに切り出し

---
+++ api/app/services/google_maps_service.rb
@@ -0,0 +1,22 @@
+class GoogleMapsService
+  require "net/http"
+  require "uri"
+
+  BASE_URL = ENV['GOOGLE_MAP_PLACE_URL']
+  API_KEY = ENV['GOOGLE_MAP_API_KEY']
+  LANGUAGE = 'ja'.freeze
+
+  def self.search_shops_by_location(location)
+    query = Shop.generate_search_query(location)
+    uri = URI.parse("#{BASE_URL}/textsearch/json?query=#{query}&key=#{API_KEY}&language=#{LANGUAGE}")
+    res = Net::HTTP.get_response(uri)
+    res.body
+  end
+
+  def self.get_shop_details(place_id)
+    fields = Shop::GOOGLE_MAP_FIELDS
+    uri = URI.parse("#{BASE_URL}/details/json?place_id=#{place_id}&fields=#{fields}&key=#{API_KEY}&language=#{LANGUAGE}")
+    res = Net::HTTP.get_response(uri)
+    res.body
+  end
+end
