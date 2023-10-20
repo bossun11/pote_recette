7aa44c4e593401b83403d946f0f3c59228004d9d
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-10-20 15:07:59 +0900

    Test: Userモデルのテストに項目を追加

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User do
   context "正常系" do
+    let!(:user) { create(:user) }
+
     it "ユーザー登録ができること" do
-      user = build(:user)
       expect(user).to be_valid
+    end
+
+    it "ユーザー情報を編集できること" do
+      user.name = "test"
+      user.email = Faker::Internet.email
+      user.image = Faker::LoremFlickr.image
+      expect(user).to be_valid
+    end
+
+    it "ユーザー削除ができること" do
+      user.destroy
+      expect(user).to be_destroyed
     end
   end
 
@@ -29,6 +42,35 @@ RSpec.describe User do
     it "メールアドレスが重複していれば登録できないこと" do
       duplicate_user = build(:user, email: user.email)
       expect(duplicate_user).not_to be_valid
+    end
+
+    it "パスワードがなければ登録できないこと" do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it "パスワードが8文字未満であれば登録できないこと" do
+      user.password = "a" * 7
+      expect(user).not_to be_valid
+    end
+
+    it "パスワードと確認用パスワードが一致しなければ登録できないこと" do
+      user.password_confirmation = "password1"
+      expect(user).not_to be_valid
+    end
+  end
+
+  context "メソッド" do
+    let!(:user) { create(:user) }
+    let!(:shop) { create(:shop) }
+
+    it "bookmark(shop)が正常に動作すること" do
+      expect { user.bookmark(shop) }.to change { user.bookmarked_shops.count }.by(1)
+    end
+
+    it "remove_bookmark(shop)が正常に動作すること" do
+      create(:bookmark, user_id: user.id, shop_id: shop.id)
+      expect { user.remove_bookmark(shop) }.to change { user.bookmarked_shops.count }.by(-1)
     end
   end
 end
